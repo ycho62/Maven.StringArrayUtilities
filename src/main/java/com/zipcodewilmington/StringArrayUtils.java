89141d9114e3c715eb5a7955e7936d2a0d91da7f
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-03-04 13:55:52 -0500

    made changes

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington;
 
+import java.sql.Array;
+import java.util.*;
+
 /**
  * Created by leon on 1/29/18.
  */
@@ -25,7 +28,7 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+        return array[array.length-1];
     }
 
     /**
@@ -33,7 +36,7 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+        return array[array.length-2];
     }
 
     /**
@@ -42,7 +45,10 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
-        return false;
+
+        List<String> list = Arrays.asList(array);
+        return list.contains(value);
+
     }
 
     /**
@@ -50,7 +56,13 @@ public class StringArrayUtils {
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+//        List<String> list = Arrays.asList(array);
+//        return Collections.list;
+        for (int i = 0; i < array.length/2; i++) {
+            String temp = array[i];
+            array[i] = array[array.length - i -1];
+            array[array.length - i -1] = temp;
+        } return array;
     }
 
     /**
@@ -58,16 +70,32 @@ public class StringArrayUtils {
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
     public static boolean isPalindromic(String[] array) {
-        return false;
-    }
+        String[] reverse = new String[100];
+        return Arrays.equals(array, reverse(array));
+//        int trueFalse;
+//        for (int i = 0; i <= array.length/2; i++) {
+//            if (Objects.equals(array[i], array[array.length - i -1])) {
+//                trueFalse =1;
+//                break;
+//            } else {
+//                trueFalse =0;
+//
+//            }
+//        }
+
+        }
+
 
     /**
      * @param array array of String objects
      * @return true if each letter in the alphabet has been used in the array
      */ // TODO
     public static boolean isPangramic(String[] array) {
-        return false;
-    }
+        String string1 = Arrays.toString(array);
+        String string2 = string1.toLowerCase();
+        return string2.chars().filter(i -> i >= 'a' && i <= 'z').distinct().count() == 26;
+
+   }
 
     /**
      * @param array array of String objects
